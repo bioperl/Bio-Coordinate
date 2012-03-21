feece49067eda9185a055b012e0938cd05edbb2a
Chris Fields <cjfields@bioperl.org>
2012-03-21 08:49:14 -0500

    untabify, remove extra spacing

--- lib/Bio/Coordinate/Result/Gap.pm
+++ lib/Bio/Coordinate/Result/Gap.pm
@@ -1,7 +1,7 @@
 #
 # BioPerl module for Bio::Coordinate::Result::Gap
 #
-# Please direct questions and support issues to <bioperl-l@bioperl.org> 
+# Please direct questions and support issues to <bioperl-l@bioperl.org>
 #
 # Cared for by Heikki Lehvaslaiho <heikki-at-bioperl-dot-org>
 #
@@ -43,9 +43,9 @@ Please direct usage questions or support issues to the mailing list:
 
 I<bioperl-l@bioperl.org>
 
-rather than to the module maintainer directly. Many experienced and 
-reponsive experts will be able look at the problem and quickly 
-address it. Please include a thorough description of the problem 
+rather than to the module maintainer directly. Many experienced and
+reponsive experts will be able look at the problem and quickly
+address it. Please include a thorough description of the problem
 with code and data examples if at all possible.
 
 =head2 Reporting Bugs
--- lib/Bio/Coordinate/Result/Match.pm
+++ lib/Bio/Coordinate/Result/Match.pm
@@ -1,7 +1,7 @@
 #
 # BioPerl module for Bio::Coordinate::Result::Match
 #
-# Please direct questions and support issues to <bioperl-l@bioperl.org> 
+# Please direct questions and support issues to <bioperl-l@bioperl.org>
 #
 # Cared for by Heikki Lehvaslaiho <heikki-at-bioperl-dot-org>
 #
@@ -19,8 +19,8 @@ Bio::Coordinate::Result::Match - Another name for Bio::Location::Simple
 =head1 SYNOPSIS
 
   $loc = Bio::Coordinate::Result::Match->new(-start=>10,
-                                            -end=>30,
-                                            -strand=>+1);
+											-end=>30,
+											-strand=>+1);
 
 =head1 DESCRIPTION
 
@@ -37,15 +37,15 @@ of the Bioperl mailing lists.  Your participation is much appreciated.
   bioperl-l@bioperl.org                  - General discussion
   http://bioperl.org/wiki/Mailing_lists  - About the mailing lists
 
-=head2 Support 
+=head2 Support
 
 Please direct usage questions or support issues to the mailing list:
 
 I<bioperl-l@bioperl.org>
 
-rather than to the module maintainer directly. Many experienced and 
-reponsive experts will be able look at the problem and quickly 
-address it. Please include a thorough description of the problem 
+rather than to the module maintainer directly. Many experienced and
+reponsive experts will be able look at the problem and quickly
+address it. Please include a thorough description of the problem
 with code and data examples if at all possible.
 
 =head2 Reporting Bugs
